625da4a04b8320fac4599a6a668aa42c7e59617b
ASY <lucky_asyxu@163.com>
2021-01-07 13:03:21 +0800

    :sparkles: (up)

--- src/config/dev.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  env: 'development',
-  server: {
-      protocol: 'https',
-      host: 'cloudapi.bytedance.net',
-      port: 443,
-      prefix: `https://cloudapi.bytedance.net/faas/services/tt3966o2mbcztlxs13/invoke`
-    },
-}
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import Taro from '@tarojs/taro'
-import config from '../config'
+import config from '../../host'
 import Global from './global'
 
 function addHeader(params, headers = {}) {
